66e8c14d65f8869c1343307df21a45d3efc265eb
Erick Araujo de Carvalho <erickbyborges@gmail.com>
2024-03-23 22:49:26 -0300

    Resolve conflict main

--- src/main/java/com/ea/libmanagement/infrastructure/security/TokenService.java
+++ src/main/java/com/ea/libmanagement/infrastructure/security/TokenService.java
@@ -1,11 +1,10 @@
 package com.ea.libmanagement.infrastructure.security;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.ea.libmanagement.domain.entities.User;
-import com.ea.libmanagement.infrastructure.configuration.SettingsHash;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -15,23 +14,37 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    private final SettingsHash settingsHash;
+    @Value("${api.security.token.secret}")
+    private String secret;
     @Autowired
-    public TokenService(SettingsHash settingsHash) {
-        this.settingsHash = settingsHash;
-    }
+    public TokenService() {}
 
     public String generateToken(User user) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256(settingsHash.getSecret());
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("libmanagement_api")
+                    .withSubject(user.getEmail())
                     .withExpiresAt(expiresDate())
                     .withClaim("name", user.getName())
                     .withClaim("role", user.getRole().toString())
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token", exception);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("libmanagement_api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }catch (JWTCreationException exception) {
+            return "";
         }
     }
 
